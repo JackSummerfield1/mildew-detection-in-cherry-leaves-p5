dc660d5512ea6958bd9a72bc46ad65ef14e94771
JackSummerfield1 <162305942+JackSummerfield1@users.noreply.github.com>
2025-04-14 15:01:58 +0000

    feat: func to resize input img to avg size

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -32,3 +32,14 @@ def plot_prediction_probs(pred_proba, pred_class):
             range_y=[0,1],
             width=800, height=400,template='seaborn')
     st.plotly_chart(fig)
+
+    def resize_input_image(img, version):
+        """
+        Reshape image to the average image size
+        """
+
+        img_shape = load_pkl_file(file_path=f"outputs/{version}/img_shape.pkl")
+        img_resized = img.resize((img_shape[1], img_shape[0]), Image.LANCZOS)
+        my_img = np.expand_dims(img_resized, axis=0)/255
+
+        return my_img
