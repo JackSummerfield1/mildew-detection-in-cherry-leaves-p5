cc247cd35f4006aa65343073f04778a3d97d97b1
JackSummerfield1 <162305942+JackSummerfield1@users.noreply.github.com>
2025-04-14 14:48:24 +0000

    feat: func to download df as csv

--- src/data_management.py
+++ src/data_management.py
@@ -4,3 +4,15 @@ import os
 import base64
 from datetime import datetime
 import joblib
+import streamlit as st
+
+
+def download_dataframe_as_csv(df):
+    datetime_now = datetime.now().strftime("%d%b%Y_%Hh%Mmin%Ss")
+    csv = df.to_csv().encode()
+    st.download_button(
+        label=f"Download Report! ({datetime_now})",
+        data=csv,
+        file_name=f"Report_{datetime_now}.csv",
+        mime='text/csv'
+    )
