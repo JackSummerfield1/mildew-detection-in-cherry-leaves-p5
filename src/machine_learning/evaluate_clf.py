7f84f6375abf31b86c8e769d2e9debc88d652af7
JackSummerfield1 <162305942+JackSummerfield1@users.noreply.github.com>
2025-04-14 14:53:05 +0000

    feat: create func to load eval on test set

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -1,2 +1,8 @@
 import streamlit as st
 from src.data_management import load_pkl_file
+
+def load_test_evaluation(version):
+    """
+    Load evaluation of the ML model on the Test set
+    """
+    return load_pkl_file(f'outputs/{version}/evaluation.pkl')
